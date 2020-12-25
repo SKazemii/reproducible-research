5dd7a09abddb1c3810e4746b73de74785b5cd118
SKazemii <Kazemi_Saeed@Hotmail.com>
2020-12-25 19:03:29 -0400

    working on dataset

---
+++ code/step.py
@@ -0,0 +1,18 @@
+import numpy as np
+import h5py
+
+with h5py.File(
+    "/Users/saeedkazemi/Documents/reproducible-research/data/colourvideo_sil.h5", "r"
+) as hdf:
+    items = list(hdf.items())
+    print(items)
+    barefoot = hdf.get("barefoot")
+    items = list(barefoot.items())
+    print(items)
+    data = np.array(barefoot.get("data"))
+    print(data.shape)
+    metadata = np.array(barefoot.get("metadata"))
+    print(metadata.shape)
+    timestamps = np.array(barefoot.get("timestamps"))
+    print(timestamps.shape)
+    print(metadata)
